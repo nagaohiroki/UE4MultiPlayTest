35119a077865386a87bf63b409825bdf706e40ac
nagaohiroki <testplayers0226@gmail.com>
2020-11-17 17:02:42 +0900

    add project

---
+++ MyProject/Source/MyProject/MyProjectGameModeBase.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProjectGameModeBase.generated.h"
+
+UCLASS()
+class MYPROJECT_API AMyProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
